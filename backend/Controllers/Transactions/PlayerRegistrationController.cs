531dc482b8b3df9fd53941c65a6640d704e5f351
Susmaher <troltom.122@gmail.com>
2025-12-17 11:29:27 +0100

    Extended

---
+++ backend/Controllers/Transactions/PlayerRegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using backend.Context;
+using backend.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers.Transactions
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerRegistrationController : ControllerBase
+    {
+        private readonly FootballDbContext _context;
+
+        public PlayerRegistrationController(FootballDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> RegisterPlayer(PlayerRegistrationDto player) 
+        {
+            await using var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                var newPlayer = 
+            }
+            catch
+            {
+                //alma
+            }
+        }
+    }
+}
---
+++ backend/Dtos/PlayerRegistrationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Dtos
+{
+    public class PlayerRegistrationDto
+    {
+        public string Name { get; set; }
+        public DateOnly Birth_date { get; set; }
+        public int PositionId { get; set; }
+        public int? TeamId { get; set; }
+    }
+}
